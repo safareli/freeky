046607164c8ab4ac6bc1c8f08bef171bc29394d1
Irakli Safareli <i.safareli@gmail.com>
2016-06-19 19:15:30 +0400

    Abstract logic of fixing `do` notation into decorator;
    To fix issue with state full generators we introduced `IGNORE_VALUE` and updated logic of `foldMap` to count for this value.
    Here I have abstracted that logic into a decorator and used this with foldMap so it now looks as before.
    It will be useful with other functions which traverse Free structure and need to ignore `IGNORE_VALUE`.

--- free.js
+++ free.js
@@ -37,18 +37,30 @@ const liftF = command => Free.Impure(command, Free.Pure)
 // Special object which should be ignored.
 const IGNORE_VALUE = {}
 
-Free.prototype.foldMap = function(interpreter, of) {
+const fixDo = function(f) {
+  return function() {
+    const context = this.cata({
+      Pure: (a) => this,
+      Impure: (intruction_of_arg, next) => {
+        if (intruction_of_arg === IGNORE_VALUE) {
+          return next()
+        } else {
+          return this
+        }
+      }
+    })
+    return f.apply(context, arguments)
+  }
+}
+
+Free.prototype.foldMap = fixDo(function(interpreter, of) {
   return this.cata({
     Pure: a => of(a),
-    Impure: (intruction_of_arg, next) => {
-      if (intruction_of_arg === IGNORE_VALUE) {
-        return next().foldMap(interpreter, of)
-      } else {
-        return interpreter(intruction_of_arg).chain(result =>
-          next(result).foldMap(interpreter, of))
-      }
-    }
+    Impure: (intruction_of_arg, next) =>
+      interpreter(intruction_of_arg).chain(result =>
+        next(result).foldMap(interpreter, of)
+      )
   })
-}
+})
 
 module.exports = { liftF, Free, IGNORE_VALUE }
